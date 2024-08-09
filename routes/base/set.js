f7d9e6f27d5f6be39ac0cffa2c451012cf126ed4
Mihail Zinchenko <Miffka82@gmail.com>
2024-08-09 17:58:11 +0300

    size value = 0

--- routes/base/set.js
+++ routes/base/set.js
@@ -110,7 +110,7 @@ function getSet (req, res) {
                                     }).then(function(windowSillsColors) {
                                       models.addition_colors.findAll({ where: {lists_type_id: 33}
                                       }).then(function(spillwaysColors) {
-
+                                        console.log('}}}}}}}}}}}}}}}}0', list);
                                         res.render('base/set', {
                                           i18n               : i18n,
                                           title              : i18n.__('Edit set'),
@@ -124,6 +124,7 @@ function getSet (req, res) {
                                           spillwaysFolders   : spillwaysFolders,
                                           visorsFolders      : visorsFolders,
                                           connectorsFolders  : connectorsFolders,
+                                          size               : list.size,
                                           // mosquitosFolders   : mosquitosFolders,
                                           doorhandlesFolders : doorhandlesFolders,
                                           handlesColors      : handlesColors,
