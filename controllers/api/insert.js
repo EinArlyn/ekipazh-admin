e0c88d509e8ffccf483b095dd780ccc4dd860b4d
Mihail Zinchenko <Miffka82@gmail.com>
2024-08-29 11:38:22 +0300

    change logic calc order Purchase Price

--- controllers/api/insert.js
+++ controllers/api/insert.js
@@ -133,7 +133,8 @@ function _setPrices(orderRow, user, factory, childPurchase, seller) {
       var userMarginPrice = 0.00;
       var constructDiscount = 0;
       var addElemDiscount = 0;
-      var userPurchasePrice = parseFloat(parseFloat(orderRow.order_price) - (parseFloat(orderRow.templates_price * (userDiscounts.max_construct / 100)) + parseFloat(orderRow.addelems_price * (userDiscounts.max_add_elem / 100)))).toFixed(2);
+      // var userPurchasePrice = parseFloat(parseFloat(orderRow.order_price) - (parseFloat(orderRow.templates_price * (userDiscounts.max_construct / 100)) + parseFloat(orderRow.addelems_price * (userDiscounts.max_add_elem / 100)))).toFixed(2);
+      var userPurchasePrice = parseFloat(parseFloat(orderRow.order_price) - (parseFloat(orderRow.templates_price / ((userDiscounts.default_construct +100 )/ 100)) + parseFloat(orderRow.addelems_price / ((userDiscounts.default_add_elem + 100) / 100)))).toFixed(2);
       var userBasePrice = parseFloat(orderRow.order_price).toFixed(2);
       var userSalePrice = parseFloat(childPurchase).toFixed(2);
       var userMountingPrice = 0.00;
